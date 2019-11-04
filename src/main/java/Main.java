ccd17e1fc3f2b54afa64925c50e405941eb080a6
Alvaro012 <aldedieg@ucm.es>
2019-11-04 16:51:11 +0100

    My first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,18 @@
 import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Main {
     public static void main(final String... args) {
-        System.out.println("Hello World");
+        System.out.println("Insert the PESEL number: ");
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        while (input.equals("")){
+            System.out.println("PESEL number not found, please try again");
+            input = in.nextLine();
+        }
+        PESEL pesel = new PESEL(input);
+        if (pesel.peselOperation(input))
+            System.out.println("mu bn");
     }
+
 }
--- src/main/java/PESEL.java
+++ src/main/java/PESEL.java
@@ -1,3 +1,13 @@
 public class PESEL {
+    public PESEL(String input) {
+        peselOperation(input);
+    }
+
     //AQUI VAN LAS OPERACIONES DE PESEL
+    public boolean peselOperation(String input) {
+        if (input.equals("Hola")) {
+            return true;
+        } else
+            return false;
+    }
 }
