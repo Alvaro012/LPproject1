7f1f727183d30d26761e5d5b70a24f9a3429b0ff
Alvaro012 <aldedieg@ucm.es>
2019-11-05 16:37:41 +0100

    My first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,14 +6,13 @@ public class Main {
         System.out.println("Insert the PESEL number: ");
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
-        while (input.equals("")){
-            System.out.println("PESEL number not found, please try again");
+        PESEL pesel = new PESEL(input);
+        while (input.equals("") || !pesel.peselOperation(input) ){
+            System.out.println("PESEL number incorrect");
             input = in.nextLine();
         }
-        PESEL pesel = new PESEL(input);
         if (pesel.peselOperation(input))
             System.out.println("PESEL number correct");
-        else System.out.println("PESEL number incorrect");
     }
 
 }
--- src/main/java/PESEL.java
+++ src/main/java/PESEL.java
@@ -1,18 +1,35 @@
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class PESEL {
     public PESEL(String input) {
         peselOperation(input);
     }
 
-    //AQUI VAN LAS OPERACIONES DE PESEL
+
     public boolean peselOperation(String input) {
         String[] splited = input.split("");
-        int[] newDigits = new int[input.length()];
-        for(int i = 0;i<input.length();i++){
-            newDigits[i] = Integer.parseInt(splited[i]);
+        List<Integer> newDigits = new ArrayList<Integer>(input.length());
+        if(input.length() < 11) {
+            return false;
         }
-        
-        return true;
+        for(int i = 0;i<input.length();i++){
+            newDigits.add(i, Integer.parseInt(splited[i]));
+        }
+        int check = (9* newDigits.get(0) + 7* newDigits.get(1) + 3* newDigits.get(2) + 1* newDigits.get(3) + 9* newDigits.get(4) + 7* newDigits.get(5) + 3* newDigits.get(6) + 1* newDigits.get(7) + 9* newDigits.get(8) + 7* newDigits.get(9));
+        if((check % 10) != newDigits.get(10))
+            return false;
+        else
+         return true;
+    }
+    public void writePesel() throws IOException {
+        InputStream initialStream = new FileInputStream(new File("C:/Users/Alvaro/IdeaProjects/LPproject1/ReadFile.txt"));
+        byte[] buffer = new byte[initialStream.available()];
+        initialStream.read(buffer);
+        File targetFile = new File("C:/Users/Alvaro/IdeaProjects/LPproject1/WriteFile.txt");
+        OutputStream outStream = new FileOutputStream(targetFile);
+        outStream.write(buffer);
     }
 }
